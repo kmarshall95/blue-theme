6e54311c4591e46f3e28b8d65bb5f3747b7a4f3f
kyle marshall <kyle05marshall2014@gmail.com>
2022-10-29 16:55:51 -0400

    skeleton nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+      import Home from './component/pages/Home';
+      import About from './component/pages/About';
+      import Services from './component/pages/Services';
+      import Portfolio from './component/pages/Portfolio';
+      import Contact from './component/pages/Contact';
+      import Layout from './component/pages/Layout';
+      import NoPage from './component/pages/NoPage';
+      
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+      
+      
+      
+      <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Layout/>}>
+                <Route index element={<Home/>}/>
+                <Route path="About" element={<About/>}/>
+                <Route path="Portfolio" element={<Portfolio/>}/>
+                <Route path="Services" element={<Services/>}/>
+                <Route path="Contact" element={<Contact/>}/>
+                <Route path="*" element={<NoPage/>}/>
+              </Route>
+            </Routes>
+          </BrowserRouter>
+      
+    );
+  };
 
 export default App;
---
+++ src/component/nav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const nav = () => {
+
+    return(
+        <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Buy</li>
+                <li>Contact</li>
+                <li>Explore</li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default nav;
